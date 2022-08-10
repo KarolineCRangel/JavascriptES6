43f229508223841cfaf4c2013497bb59e143c6d7
KarolineCRangel <karolinecrangel@gmail.com>
2022-08-10 16:27:06 -0300

    Variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,18 @@
+//Variáveis
+var nome = 'Karoline';
+var idade = 28;
+var estuda = true;
+
+//consele.log() é usado para printar no console.
+console.log(nome, idade, estuda);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(totalPreco)
+
+var sobrenome = 'Carregal',
+    cidade = 'Campos';
+
+console.log(sobrenome, cidade);
