01b3246b5c54f72ddaa7ad69535d5eae41ad1f0e
KarolineCRangel <karolinecrangel@gmail.com>
2022-08-11 19:36:36 -0300

    fundamentos

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,18 +1,30 @@
-//Variáveis
-var nome = 'Karoline';
-var idade = 28;
-var estuda = true;
+//VARIÁVEIS
+var nome = 'Karoline',
+    idade = 28,
+    comidaFavorita;
 
+comidaFavorita = 'Parmegiana'
 //consele.log() é usado para printar no console.
-console.log(nome, idade, estuda);
+console.log(nome, idade, comidaFavorita);
 
-var preco = 25;
-var totalComprado = 5;
-var totalPreco = preco * totalComprado;
 
-console.log(totalPreco)
 
-var sobrenome = 'Carregal',
-    cidade = 'Campos';
+// DIFERENÇA ENTRE VAR, LET E CONST
+//Hoisting - move as variáveis para o topo do escopo antes da execução do código.
 
-console.log(sobrenome, cidade);
+//var pode ser declarada novamente dentro do escopo
+let time,
+    torcida,
+    jogador,
+    arbitro,
+    narrador;
+
+
+//let não pode ser declarada mais de uma vez, mas pode ser atualizada. é uma variáel de bloco, portando só funciona dentro do bloco proposto.
+let estado = 'Rio de Janeiro';
+
+//Const não pode ser declaradas mais de uma vez e nem atualizadas
+const pi = 1.14;
+
+console.log(time, estado, pi);
+
