e2398a40269a499308373d968f543d33570127ed
KarolineCRangel <karolinecrangel@gmail.com>
2022-08-12 18:01:55 -0300

    Fundamentos Javascript

---
+++ Fundamentos/Objeto2/script.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de string
+// fixed
+// length
+// replace
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+//btn.innerHTML
+//btn.id
+//btn.addEventListener
+//btn.appendChild
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+//Clipboard.js
